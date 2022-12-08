d5f0370eadcff319e78c05a5f01097668961c8bf
creepysmilejpg <rdonner03@gmail.com>
2022-12-08 14:04:45 -0600

    Needed to add: - fix box claiming (Done) - Start button (Done) - Reset button (Done) - Potentially separate classes (Decided wasn't necessary)

--- first.py
+++ first.py
@@ -8,10 +8,32 @@ x = 2
 player1 = []
 player2 = []
 
-'''
-start button 
-reset button
-'''
+
+def start():
+    button1['state'] = 'active'
+    button2['state'] = 'active'
+    button3['state'] = 'active'
+    button4['state'] = 'active'
+    button5['state'] = 'active'
+    button6['state'] = 'active'
+    button7['state'] = 'active'
+    button8['state'] = 'active'
+    button9['state'] = 'active'
+    button11['state'] = 'active'
+
+
+def reset():
+    player1 = []
+    player2 = []
+    button1['text'] = ''
+    button2['text'] = ''
+    button3['text'] = ''
+    button4['text'] = ''
+    button5['text'] = ''
+    button6['text'] = ''
+    button7['text'] = ''
+    button8['text'] = ''
+    button9['text'] = ''
 
 
 def define_sign(num):
@@ -295,37 +317,40 @@ def bot_click():
 
 root = Tk()
 
-label1 = Label(root, text="Player 1: X", font="times 15 bold")
-label1.grid(row=0, column=1)
-
-label2 = Label(root, text="Player 2: O", font="times 15 bold")
-label2.grid(row=0, column=3)
-
-button1 = Button(root, width=20, height=10, command=lambda: define_sign(1))
+button1 = Button(root, width=20, height=10, command=lambda: define_sign(1), state=DISABLED)
 button1.grid(row=1, column=1)
 
-button2 = Button(root, width=20, height=10, command=lambda: define_sign(2))
+button2 = Button(root, width=20, height=10, command=lambda: define_sign(2), state=DISABLED)
 button2.grid(row=1, column=2)
 
-button3 = Button(root, width=20, height=10, command=lambda: define_sign(3))
+button3 = Button(root, width=20, height=10, command=lambda: define_sign(3), state=DISABLED)
 button3.grid(row=1, column=3)
 
-button4 = Button(root, width=20, height=10, command=lambda: define_sign(4))
+button4 = Button(root, width=20, height=10, command=lambda: define_sign(4), state=DISABLED)
 button4.grid(row=4, column=1)
 
-button5 = Button(root, width=20, height=10, command=lambda: define_sign(5))
+button5 = Button(root, width=20, height=10, command=lambda: define_sign(5), state=DISABLED)
 button5.grid(row=4, column=2)
 
-button6 = Button(root, width=20, height=10, command=lambda: define_sign(6))
+button6 = Button(root, width=20, height=10, command=lambda: define_sign(6), state=DISABLED)
 button6.grid(row=4, column=3)
 
-button7 = Button(root, width=20, height=10, command=lambda: define_sign(7))
+button7 = Button(root, width=20, height=10, command=lambda: define_sign(7), state=DISABLED)
 button7.grid(row=8, column=1)
 
-button8 = Button(root, width=20, height=10, command=lambda: define_sign(8))
+button8 = Button(root, width=20, height=10, command=lambda: define_sign(8), state=DISABLED)
 button8.grid(row=8, column=2)
 
-button9 = Button(root, width=20, height=10, command=lambda: define_sign(9))
+button9 = Button(root, width=20, height=10, command=lambda: define_sign(9), state=DISABLED)
 button9.grid(row=8, column=3)
+
+button10 = Button(root, text="Start", width=20, height=3, command=lambda: start())
+button10.grid(row=0, column=1)
+
+button11 = Button(root, text="Reset", width=20, height=3, command=lambda: reset(), state=DISABLED)
+button11.grid(row=0, column=3)
+
+button12 = Button(root, width=20, height=3, command=lambda: start(), state=DISABLED)
+button12.grid(row=0, column=2)
 
 root.mainloop()
